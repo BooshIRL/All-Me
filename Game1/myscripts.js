15ef315cdde868af8f16b5962d166087c3d8941f
BooshIRL <105866045+BooshIRL@users.noreply.github.com>
2023-06-14 20:10:15 -0500

    BrickBreak

--- Game1/myscripts.js
+++ Game1/myscripts.js
@@ -6,3 +6,15 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+function draw() {
+  ctx.beginPath();
+  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "@0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+setInterval(draw, 10);
