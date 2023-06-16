9c716c14bbfaa25db4c40460410d6d0e1ce779ba
BooshIRL <105866045+BooshIRL@users.noreply.github.com>
2023-06-16 15:47:58 -0500

    Bouncing Baby Ball

--- Game1/myscripts.js
+++ Game1/myscripts.js
@@ -9,12 +9,31 @@ function myFunction() {
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+const ballRadius = 10;
 
-function draw() {
+function drawBall() {
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "@0095DD";
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#8a2be2";
   ctx.fill();
   ctx.closePath();
 }
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  if (y + dy > canvas.height || y + dy < ballRadius) {
+    dy = -dy;
+  }
+  if (x + dx > canvas.width || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  x += dx;
+  y += dy
+}
+
 setInterval(draw, 10);
