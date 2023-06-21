72cdf2fab3b4c36994e2ac068337b5c3d57eebe5
BooshIRL <105866045+BooshIRL@users.noreply.github.com>
2023-06-20 21:41:43 -0500

    Block Collision

--- Game1/myscripts.js
+++ Game1/myscripts.js
@@ -33,8 +33,8 @@ var bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for (let r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = {x: 0, y: 0};
-  }0
+    bricks[c][r] = {x: 0, y: 0, status: 1};
+  }
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -57,9 +57,15 @@ function keyUpHandler(e) {
 }
 
 function collisionDetection() {
-  for (let c = 0; c < brickColumnCount; c++;) {
-    for (let r = 0; r < brickColumnCount; r++) {
-      const b = bricks[c][rS]
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status == 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
     }
   }
 }
@@ -83,15 +89,17 @@ function drawPaddle() {
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
-      var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-      var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-      bricks[c][r].x =  brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "red";
-      ctx.fill();
-      ctx.closePath;
+      if (bricks[c][r].status == 1) {
+       var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+       var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+       bricks[c][r].x = brickX;
+       bricks[c][r].y = brickY;
+       ctx.beginPath();
+       ctx.rect(brickX, brickY, brickWidth, brickHeight);
+       ctx.fillStyle = "red";
+       ctx.fill();
+       ctx.closePath;
+      }
     }
   }
 }
@@ -101,6 +109,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
